001f55661f7cf14c657e96343ae3c1398799f0e5
Tushar <tushard3998@gmail.com>
2024-02-07 10:55:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+console.log("JavaScript Conditionals: if, else if, else ladder ")
+
+//If Else
+
+/*let age = 11;
+
+if(age>18){
+    console.log("You can drive")
+}
+else{
+    console.log("You can not drive")
+}*/
+
+//Arithmetic Operators
+
+/*let age = 9;
+let grace = 3;
+
+console.log(age-grace)
+console.log(age+grace)
+console.log(age*grace)
+console.log(age/grace)
+console.log(age**grace)
+console.log(age%grace)
+console.log(age++)
+console.log(age--)*/
+
+let age = 20;
+
+/*if(age>0){
+    console.log(" A valid Age")
+}*/
+if(age>10 && age<15){
+    console.log("You can not drive.")
+}
+else if(age>18){
+    console.log("You can drive")
+}
+else {
+    console.log("Invalid Number")
+}
+
+let a =33;
+let b = 99;
+
+let c = a<b ? (a+b) : (a-b)
+
+console.log(c)
